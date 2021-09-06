2fff72f1127858a4e0b13a41264216839f0637d0
Eyber Aguirre <eaguirre@globalnexo.com>
2021-09-06 16:31:32 -0400

    create components: ListCharacter CardCharacter Adding styles

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,32 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+	state: {
+		characters: [],
+		charactersFilter: []
+	},
+	mutations: {
+		setCharacters(state, payload) {
+			state.characters = payload;
+		},
+		setCharactersFilter(state, payload) {
+			state.charactersFilter = payload;
+		}
+	},
+	actions: {
+		async getCharacters({commit}) {
+			try {
+				const response = await fetch('https://rickandmortyapi.com/api/character')
+				const data = await response.json();
+				console.log(data.results);
+				commit('setCharacters', data.results)
+				commit('setCharactersFilter', data.results)
+			}
+			catch(error) {
+				console.error(error);
+			}
+		}
+	},
+	modules: {
+	}
 })
